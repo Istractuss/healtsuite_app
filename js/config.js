9f7cd75644f7f157a554217b9e86232b99e6afc0
Istractuss <190735195+Istractuss@users.noreply.github.com>
2025-09-01 02:33:36 +0200

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,7 @@
+// config.js
+// ⚠️ No subas este archivo a GitHub ni lo compartas públicamente (tu API Key quedaría expuesta)
+
+export const GEMINI_CONFIG = {
+  API_KEY: "AIzaSyDCtuKFau27WR70n9L4OY8JakqcnBtvH_M",
+  URL: "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent"
+};
